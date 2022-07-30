109b84cf7b3020722344892689a06280768c18de
yokesh7373@gmail.com <gitHub@1>
2022-07-30 20:01:16 +0530

    Created Song Entity

---
+++ app/src/main/java/com/training/spotifyclone/data/entities/Song.java
@@ -0,0 +1,39 @@
+package com.training.spotifyclone.data.entities;
+
+public class Song {
+
+    private String mediaId;
+    private String title;
+    private String subtitle;
+    private String songUrl;
+    private String imageUrl;
+
+    public Song(String mediaId, String title, String subtitle, String songUrl, String imageUrl) {
+        this.mediaId = mediaId;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.songUrl = songUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public String getSongUrl() {
+        return songUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+}
